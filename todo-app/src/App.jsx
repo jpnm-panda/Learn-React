402b346018e0c313760120b2dd5ad00f17f73002
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-20 05:26:02 +0900

    todoアプリの雛形を調整

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,40 +1,37 @@
-import React, { useEffect, useState } from "react";
-import ColofulMessage from "./components/ColorfulMessage";
+import React from "react";
+import "./styles.css";
 
-const App = () => {
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(false);
-
-  const onClickCountUP = () => {
-    setNum(num + 1);
-  };
-  const onClickSwitchFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  useEffect(() => {
-    if (num > 0) {
-      if (num % 3 === 0) {
-        faceShowFlag || setFaceShowFlag(true);
-      } else {
-        faceShowFlag && setFaceShowFlag(false);
-      }
-    }
-    // eslint-disable-next-line react-hocks/exhaustive-deps
-  }, [num]);
-
+export const App = () => {
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
-      <ColofulMessage color="pink">元気です！</ColofulMessage>
-      <button onClick={onClickCountUP}>カウントアップ!</button>
-      <br />
-      <button onClick={onClickSwitchFlag}>on/off</button>
-      <p>{num}</p>
-      {faceShowFlag && <p>＼(^o^)／</p>}
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li className="list-row">
+            <p>hogehoge</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li className="list-row">
+            <p className="title">hogehoge</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p>完了のTODO</p>
+        <ul>
+          <li className="list-row">
+            <p>hogehoge</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
-
-export default App;
