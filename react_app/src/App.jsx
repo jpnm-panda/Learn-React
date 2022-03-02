0cac1f78f4c5598d3e4965276379ba5eb683270e
jpnm-panda <jpnm0415shkm@gmail.com>
2022-03-02 21:20:37 +0900

    createStoreでTodoのデータを扱ってみる

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,88 +1,20 @@
 import React, { useState, useReducer } from "react";
 import "../src/index.css"
-import { InputTodo } from "../src/components/inputTodo";
-import { IncompleteTodos } from "../src/components/IncompleteTodos";
-import { CompleteTodos } from "../src/components/Complete";
-import AppContext from './context/AppContext';
-import B from './components/atoms/B';
-import BasicReducer from './components/atoms/BasicReducer';
-import C from "./components/atoms/C";
+import { TodoItem } from "./components/organism/TodoItem";
+import { CompleteTodoList } from "./components/organism/CompleteTodoList";
+import { IncompleteTodoList } from "./components/organism/IncompleteTodoList";
 
+// import { TodoItem } from "./components/molecules/TodoItem"
 
-const initialState = 0
-const reducer = (currentState, action) => {
-  switch(action) {
-    case 'add_1':
-      return currentState + 1
-    case 'multiple_3':
-      return currentState * 3
-    case 'reset':
-      return initialState
-    default:
-      return currentState
-  }
-}
 
 export const App = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState([]);
-  const [completeTodos, setCompleteTodos] = useState([]);
-  const [todoText, setTodoText] = useState("");
-  const onChangeTodoText = (event) => setTodoText(event.target.value);
-  const onClickAdd = () => {
-    if (todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newTodos);
-    setTodoText("");
-  };
-  const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setIncompleteTodos(newTodos);
-  };
-  const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index, 1);
-
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setIncompleteTodos(newIncompleteTodos);
-    setCompleteTodos(newCompleteTodos);
-  };
-  const onClickBack = (index) => {
-    const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index, 1);
-
-    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
-    setCompleteTodos(newCompleteTodos);
-    setIncompleteTodos(newIncompleteTodos);
-  };
-  const [count, dispatch] = useReducer(reducer, initialState)
+  // const todos = useSelector((state) => state.todos);
 
   return (
     <>
-      <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
-        Count {count}
-        <B />
-      </AppContext.Provider>
-        
-      <InputTodo
-        todoText={todoText}
-        text={"追加"}
-        onChange={onChangeTodoText}
-        onClick={onClickAdd}
-        disabled={incompleteTodos.length >= 5}
-      />
-      {incompleteTodos.length >= 5 && (
-        <p style={{ color: "red" }}>
-          登録できるtodoは5個までやで！消化してや！
-        </p>
-      )}
-      <IncompleteTodos
-        todos={incompleteTodos}
-        onClickComplete={onClickComplete}
-        onClickDelete={onClickDelete}
-      />
-        <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+      <div>hoge</div> 
+      <CompleteTodoList></CompleteTodoList>
+      <IncompleteTodoList></IncompleteTodoList>
     </>
   );
 };
-
