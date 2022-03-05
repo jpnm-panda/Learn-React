b566e330b830aecc8776235d12d7977f495b0a08
jpnm-panda <jpnm0415shkm@gmail.com>
2022-03-05 21:33:48 +0900

    Reduxを使って削除機能をじっそう

--- react_app/src/action.jsx
+++ react_app/src/action.jsx
@@ -1,3 +1,4 @@
+// 追加機能のAction を定義
 export const ADD_TODO = "ADD_TODO";
 
 export const addTodo = (text) => ({
@@ -5,13 +6,22 @@ export const addTodo = (text) => ({
     payload: {
         id: Math.random(),
         text: text,
-        complete: false,
+        complete: false
     },
 });
 
+// 完了機能のAction を定義
 export const COMPLETED_TODO = "COMPLETED_TODO";
 
 export const completedTodo = (id) => ({
     type: COMPLETED_TODO,
+    payload: id
+});
+
+// 削除機能のAction を定義
+export const DELETED_TODO = "Deleted_TODO";
+
+export const deletedTodo = (id) => ({
+    type: DELETED_TODO,
     payload: id
 });
